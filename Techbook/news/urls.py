3a1a85a92a7b22331d527d59942024a2303a1a62
shardu1 <121335970+shardu1@users.noreply.github.com>
2023-07-09 21:23:53 +0530

    Delete Techbook/news directory

--- Techbook/news/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views 
-
-urlpatterns=[
-    path("",views.news_index,name='news_index'),
-    path('post-page.html', views.post_page ,name="post-page")
-]
--- Techbook/news/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def news_index(request):
-    return render(request,'index.html')
-
-def post_page(request):
-    return render(request,'post-page.html')
