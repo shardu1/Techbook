cfc83beb6a722c7316fba7367693bd4a4b1ef9d7
shardu1 <121335970+shardu1@users.noreply.github.com>
2023-07-09 21:23:38 +0530

    Delete Techbook/blogs directory

--- Techbook/blogs/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views 
-
-urlpatterns=[
-    path("",views.blogs_index,name='blogs_index')
-]
--- Techbook/blogs/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def blogs_index(request):
-    return render(request,'index.html')
