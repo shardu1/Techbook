030b9e114bae849bb3954064e13643d523650c9a
shardu1 <shardulmhetre868@gmail.com>
2023-07-14 11:47:35 +0530

    using template inheritacne

--- home/views.py
+++ home/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request,'techbook/html/t-index.html')
+    return render(request,'home/html/t-index.html')
 
